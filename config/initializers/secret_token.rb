6a248249743990887ef0ec121225cb04a5d4c163
Keith Thompson <keith@brilliantfantastic.com>
2013-06-04 07:52:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArIntro::Application.config.secret_token = '835f02a9559fba8bc9e21f8cf20541cb60114b9791de7ab984b0a1ded7d920a4b3b1cabbd212e5a5f96e50e49ed8ea76a09560476da31f4223c7264ba5ab3608'
